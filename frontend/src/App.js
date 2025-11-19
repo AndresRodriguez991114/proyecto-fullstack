130a75106ef7719dc33471d934d41f69e72df947
Andres Rodriguez <ayesidrodriguez@ucundinamarca.edu.co>
2025-11-19 00:08:34 -0500

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,7 @@
-import React, { useState } from "react";
 import LoginForm from "./components/LoginForm";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  const handleLogin = (userData) => {
-    setUser(userData);
-  };
-
-  return (
-    <div>
-      {!user ? (
-        <LoginForm onLogin={handleLogin} />
-      ) : (
-        <h1>Bienvenido {user.nombre}</h1>
-      )}
-    </div>
-  );
+  return <LoginForm />;
 }
 
 export default App;
